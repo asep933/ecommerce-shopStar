8d383905cd8425848eec8e56964182b21645bf7f
sepsupriatnaa <asep93339@gmail.com>
2024-03-01 22:39:32 +0700

    samaa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Dashboard = (): any => {
 
   return (
     <Case>
-      <div onClick={() => setStatus("non-active")}>
+      <div>
         <section>
           <Hero />
         </section>
